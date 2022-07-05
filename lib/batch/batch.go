b4047455cc3540486bd20fb1599f3af54bf7dd27
ihor_ostapovych <thor.ostapovych@danfoss.com>
2022-07-05 11:18:15 +0300

    feat: worker pool implementation

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -13,6 +13,29 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
+func worker(jobs <-chan int64, results chan<- user) {
+	for id := range jobs {
+		results <- getOne(id)
+	}
+}
+
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	num := int(n)
+	jobs := make(chan int64, num)
+	results := make(chan user, num)
+
+	for j := int64(0); j < pool; j++ {
+		go worker(jobs, results)
+	}
+
+	for i := int64(0); i < n; i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	for u := 0; u < num; u++ {
+		res = append(res, <-results)
+	}
+
+	return
 }
